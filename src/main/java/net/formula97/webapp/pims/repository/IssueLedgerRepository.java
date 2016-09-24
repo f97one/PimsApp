1828f3741489ce6bf70f172c0245cb2c1bd6bc70
HAJIME Fukuna <f97one@gmail.com>
2016-09-24 20:18:44 +0900

    JPQLによるクエリの書き方に誤りがあり、正しくO/Rマッピングできていなかった件を修正。

--- src/main/java/net/formula97/webapp/pims/repository/IssueLedgerRepository.java
+++ src/main/java/net/formula97/webapp/pims/repository/IssueLedgerRepository.java
@@ -1,6 +1,9 @@
 package net.formula97.webapp.pims.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import net.formula97.webapp.pims.domain.IssueLedger;
@@ -12,4 +15,6 @@ import net.formula97.webapp.pims.domain.IssueLedger;
 @Repository
 public interface IssueLedgerRepository extends JpaRepository<IssueLedger, Integer> {
 
+    @Query("SELECT o FROM IssueLedger o WHERE o.isPublic = true ORDER BY ledgerId")
+    List<IssueLedger> findByPublicLedger();
 }
