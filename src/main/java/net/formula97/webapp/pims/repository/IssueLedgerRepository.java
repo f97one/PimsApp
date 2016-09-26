47f38b67c7e4e8236cf5020ff1e0faaa6bf56a57
HAJIME Fukuna <f97one@gmail.com>
2016-09-26 23:58:57 +0900

    試験的にSpecificationsを使えるよう、JpaSpecificationExecutorを継承。

--- src/main/java/net/formula97/webapp/pims/repository/IssueLedgerRepository.java
+++ src/main/java/net/formula97/webapp/pims/repository/IssueLedgerRepository.java
@@ -3,6 +3,7 @@ package net.formula97.webapp.pims.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,8 +14,17 @@ import net.formula97.webapp.pims.domain.IssueLedger;
  *
  */
 @Repository
-public interface IssueLedgerRepository extends JpaRepository<IssueLedger, Integer> {
+public interface IssueLedgerRepository extends JpaRepository<IssueLedger, Integer>, JpaSpecificationExecutor<IssueLedger> {
 
+    /*
+     * @Query アノテーションは普通のSQLではなくJPQL(Java Persistence Query Language) で書く必要が
+     * ある。
+     * SQLはロー/カラム形式のテーブルとして結果が返るが、JPQLはオブジェクトベースなので、基本的にエンティティのコレクションで
+     * 結果が返る。
+     * また、JPQLは大文字/小文字を基本的に区別しないが、クラス/フィールドの名称はすべて元となるクラスの定義に従って
+     * 書く必要があることに注意。
+     */
+    
     @Query("SELECT o FROM IssueLedger o WHERE o.isPublic = true ORDER BY ledgerId")
     List<IssueLedger> findByPublicLedger();
 }
