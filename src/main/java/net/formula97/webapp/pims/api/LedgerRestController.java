49590859dfd50ad39f90b149f379dac59452f9d3
HAJIME Fukuna <f97one@gmail.com>
2016-09-27 00:00:28 +0900

    Serviceクラスを経由してデータアクセスするよう修正。

--- src/main/java/net/formula97/webapp/pims/api/LedgerRestController.java
+++ src/main/java/net/formula97/webapp/pims/api/LedgerRestController.java
@@ -5,10 +5,13 @@ package net.formula97.webapp.pims.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.formula97.webapp.pims.domain.IssueLedger;
+import net.formula97.webapp.pims.service.IssueLedgerService;
 
 /**
  * @author f97one
@@ -17,10 +20,13 @@ import net.formula97.webapp.pims.domain.IssueLedger;
 @RestController
 @RequestMapping("api/ledger")
 public class LedgerRestController {
+    
+    @Autowired
+    IssueLedgerService issueLedgerSvc;
 
-    List<IssueLedger> getLedger() {
-        
-        return null;
+    @RequestMapping(method = RequestMethod.GET)
+    List<IssueLedger> getPublicLedger() {
+        return issueLedgerSvc.getPublicLedgers();
     }
     
 }
---
+++ src/main/java/net/formula97/webapp/pims/service/IssueLedgerService.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package net.formula97.webapp.pims.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import net.formula97.webapp.pims.domain.IssueLedger;
+import net.formula97.webapp.pims.repository.IssueLedgerRepository;
+
+/**
+ * @author f97one
+ *
+ */
+@Service
+public class IssueLedgerService {
+
+    @Autowired
+    IssueLedgerRepository issueLedgerRepository;
+    
+    public List<IssueLedger> getPublicLedgers() {
+        return issueLedgerRepository.findAll(Specifications.where(IssueLedgerSpecifications.isPublicSpecified(true)));
+    }
+    
+    static class IssueLedgerSpecifications {
+        
+        static Specification<IssueLedger> nameContains(String ledgerName) {
+            return StringUtils.isEmpty(ledgerName) ? null : (root, query, cb) -> cb.like(root.get("ledgerName"), "%" + ledgerName + "%");
+        }
+        
+        static Specification<IssueLedger> openStatusSpecified(Integer openStatusId) {
+            return openStatusId == null ? null : (root, query, cb) -> cb.equal(root.get("openStatus"), openStatusId);
+        }
+        
+        static Specification<IssueLedger> isPublicSpecified(Boolean isPublic) {
+            return isPublic == null ? null : (root, query, cb) -> cb.equal(root.get("isPublic"), isPublic);
+        }
+    }
+}
