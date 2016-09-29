903ed3749718fffc95007b32ea037f686dc3ff36
HAJIME Fukuna <f97one@gmail.com>
2016-09-29 22:53:32 +0900

    Spring Security 4と連携できる設定を追加。

---
+++ src/main/java/net/formula97/webapp/pims/web/LoginController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.formula97.webapp.pims.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author f97one
+ *
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping("loginForm")
+    String loginForm() {
+        return "loginForm";
+    }
+}
