1d5e53a220e6258bf3826da2f88b5d7d0f62ef97
HAJIME Fukuna <f97one@gmail.com>
2016-10-02 18:58:22 +0900

    定数格納用クラスをとりあえず用意。

---
+++ src/main/java/net/formula97/webapp/pims/misc/AppConstants.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.formula97.webapp.pims.misc;
+
+/**
+ * @author f97one
+ *
+ */
+public class AppConstants {
+
+    public class SysConfig {
+        public static final String APP_TITLE = "AppTitle";
+    }
+    
+}
