a1d028357709d65fee33220f48457704a25eaa72
HAJIME Fukuna <f97one@gmail.com>
2016-10-02 19:09:45 +0900

    基底クラスにぶら下がるFormを追加していなかった件を修正。

---
+++ src/main/java/net/formula97/webapp/pims/web/forms/HeaderForm.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package net.formula97.webapp.pims.web.forms;
+
+import net.formula97.webapp.pims.domain.Users;
+
+/**
+ * @author f97one
+ *
+ */
+public class HeaderForm {
+
+    private Users users;
+
+    /**
+     * @return the users
+     */
+    public Users getUsers() {
+        return users;
+    }
+
+    /**
+     * @param users the users to set
+     */
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+    
+    
+}
