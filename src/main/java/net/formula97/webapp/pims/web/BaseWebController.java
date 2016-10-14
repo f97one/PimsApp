961bf53f757e7fd492331643075e126cefea481b
HAJIME Fukuna <f97one@gmail.com>
2016-10-14 23:27:11 +0900

    Principalの解釈方法に誤りがあり、ユーザー情報の取得が正しくできていなかった件を修正。

--- src/main/java/net/formula97/webapp/pims/web/BaseWebController.java
+++ src/main/java/net/formula97/webapp/pims/web/BaseWebController.java
@@ -8,6 +8,7 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -38,7 +39,7 @@ public class BaseWebController {
     protected Users getUserState(Model model, Principal principal) {
         Users users = null;
         
-        if (principal instanceof UserDetails) {
+        if (principal != null) {
             Authentication auth = (Authentication) principal;
             AuthorizedUsers authUsers = (AuthorizedUsers) auth.getPrincipal();
             
