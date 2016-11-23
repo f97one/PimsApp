de3971ccfa37965c4c91903f5137b66bc00cd64c
HAJIME Fukuna <f97one@gmail.com>
2016-11-23 17:50:54 +0900

    SpecificationExecutorの指定に誤りがあった件を修正。

--- src/main/java/net/formula97/webapp/pims/repository/SystemConfigRepository.java
+++ src/main/java/net/formula97/webapp/pims/repository/SystemConfigRepository.java
@@ -3,18 +3,17 @@
  */
 package net.formula97.webapp.pims.repository;
 
+import net.formula97.webapp.pims.domain.SystemConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import net.formula97.webapp.pims.domain.SystemConfig;
 
 /**
  * @author f97one
  *
  */
 @Repository
-public interface SystemConfigRepository extends JpaRepository<SystemConfig, String>, JpaSpecificationExecutor<String> {
+public interface SystemConfigRepository extends JpaRepository<SystemConfig, String>, JpaSpecificationExecutor<SystemConfig> {
 
     SystemConfig findByConfigKey(String configKey);
 }
