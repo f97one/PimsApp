528e2336c16fec93c4b07447b96a482e6aaceb57
HAJIME Fukuna <f97one@gmail.com>
2016-12-18 22:32:24 +0900

    とりあえずユーザー検索のフロントを追加。

--- src/main/java/net/formula97/webapp/pims/web/forms/UserSearchConditionForm.java
+++ src/main/java/net/formula97/webapp/pims/web/forms/UserSearchConditionForm.java
@@ -1,12 +1,12 @@
 package net.formula97.webapp.pims.web.forms;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
@@ -14,7 +14,6 @@ import java.io.Serializable;
  * Created by f97one on 2016/12/16.
  */
 @Data
-@NoArgsConstructor
 @Component
 @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserSearchConditionForm implements Serializable {
@@ -23,4 +22,34 @@ public class UserSearchConditionForm implements Serializable {
      * シリアライズUID
      */
     private static final long serialVersionUID = -1396281996738099495L;
+
+    /**
+     * 検索キーワード：ユーザー名
+     */
+    @Size(max = 32)
+    private String username;
+
+    /**
+     * 検索キーワード：表示名
+     */
+    @Size(max = 128)
+    private String displayName;
+
+    /**
+     * 検索キーワード：メールアドレス
+     */
+    @Size(max = 128)
+    private String mailAddress;
+
+    /**
+     * 検索キーワード：有効なユーザーのみに限定
+     */
+    private Boolean limitEnabledUser;
+
+    public UserSearchConditionForm() {
+        this.username = null;
+        this.displayName = null;
+        this.mailAddress = null;
+        this.limitEnabledUser = false;
+    }
 }
