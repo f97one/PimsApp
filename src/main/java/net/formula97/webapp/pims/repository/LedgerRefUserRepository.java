1a3f60a3741f27d87a359e3e242a811be8abb452
HAJIME Fukuna <f97one@gmail.com>
2017-01-22 00:19:58 +0900

    Specificationsが実装されていなかった件を修正。

--- src/main/java/net/formula97/webapp/pims/repository/LedgerRefUserRepository.java
+++ src/main/java/net/formula97/webapp/pims/repository/LedgerRefUserRepository.java
@@ -1,16 +1,16 @@
 package net.formula97.webapp.pims.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import net.formula97.webapp.pims.domain.LedgerRefUser;
 import net.formula97.webapp.pims.domain.LedgerRefUserPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author f97one
  *
  */
 @Repository
-public interface LedgerRefUserRepository extends JpaRepository<LedgerRefUser, LedgerRefUserPK> {
+public interface LedgerRefUserRepository extends JpaRepository<LedgerRefUser, LedgerRefUserPK>, JpaSpecificationExecutor<LedgerRefUser> {
 
 }
