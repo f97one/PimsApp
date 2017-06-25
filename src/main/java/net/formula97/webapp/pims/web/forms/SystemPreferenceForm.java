de66ba0fcbd7806ce9c9bfc6205412f4c412a098
HAJIME Fukuna <f97one@gmail.com>
2017-06-25 10:58:28 +0900

    とりあえずシステム設定用Formを追加。

---
+++ src/main/java/net/formula97/webapp/pims/web/forms/SystemPreferenceForm.java
@@ -0,0 +1,16 @@
+package net.formula97.webapp.pims.web.forms;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by f97one on 17/04/20.
+ */
+@Data
+@NoArgsConstructor
+public class SystemPreferenceForm {
+    /**
+     * アプリ名称
+     */
+    private String appName;
+}
