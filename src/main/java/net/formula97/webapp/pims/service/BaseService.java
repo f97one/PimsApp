b99b01b316977051d436a1910c18471ddafba05b
HAJIME Fukuna <f97one@gmail.com>
2018-05-03 20:05:00 +0900

    IDがnullの場合が未考慮だったのでnullだった場合の処理を追加。

--- src/main/java/net/formula97/webapp/pims/service/BaseService.java
+++ src/main/java/net/formula97/webapp/pims/service/BaseService.java
@@ -1,14 +1,18 @@
 package net.formula97.webapp.pims.service;
 
 import net.formula97.webapp.pims.domain.*;
+import net.formula97.webapp.pims.misc.CommonsStringUtils;
 import net.formula97.webapp.pims.repository.*;
+import net.formula97.webapp.pims.web.forms.UserSearchConditionForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specifications;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Created by f97one on 2016/11/13.
@@ -31,25 +35,41 @@ public abstract class BaseService {
 
     private Map<String, String> sysConfigMap;
 
-    protected String findCategoryById(List<CategoryMaster> categoryMasterList, int categoryId) {
+    protected String findCategoryById(List<CategoryMaster> categoryMasterList, Integer categoryId) {
+        if (categoryId == null) {
+            return "";
+        }
+
         Optional<CategoryMaster> categoryMasterOptional = categoryMasterList.stream().filter((r) -> r.getCategoryId() == categoryId).findFirst();
 
         return categoryMasterOptional.isPresent() ? categoryMasterOptional.get().getCategoryName() : "";
     }
 
-    protected String findProcessById(List<ProcessMaster> processMasterList, int processId) {
+    protected String findProcessById(List<ProcessMaster> processMasterList, Integer processId) {
+        if (processId == null) {
+            return "";
+        }
+
         Optional<ProcessMaster> processMasterOptional = processMasterList.stream().filter((r) -> r.getProcessId() == processId).findFirst();
 
         return processMasterOptional.isPresent() ? processMasterOptional.get().getProcessName() : "";
     }
 
-    protected String findSeverityById(List<SevereLevelMaster> severeLevelMasterList, int severeLevelId) {
+    protected String findSeverityById(List<SevereLevelMaster> severeLevelMasterList, Integer severeLevelId) {
+        if (severeLevelId == null) {
+            return "";
+        }
+
         Optional<SevereLevelMaster> severeLevelMasterOptional = severeLevelMasterList.stream().filter((r) -> r.getSevereLevelId() == severeLevelId).findFirst();
 
         return severeLevelMasterOptional.isPresent() ? severeLevelMasterOptional.get().getSevereLevel() : "";
     }
 
-    protected String findStatusById(List<StatusMaster> statusMasterList, int statusId) {
+    protected String findStatusById(List<StatusMaster> statusMasterList, Integer statusId) {
+        if (statusId == null) {
+            return "";
+        }
+
         Optional<StatusMaster> statusMasterOptional = statusMasterList.stream().filter((r) -> r.getStatusId() == statusId).findFirst();
 
         return statusMasterOptional.isPresent() ? statusMasterOptional.get().getStatusName() : "";
@@ -82,4 +102,50 @@ public abstract class BaseService {
         return usersOpt.isPresent() ? usersOpt.get().getDisplayName() : "";
     }
 
+    /**
+     * @author f97one
+     */
+    @Service
+    public static class AuthorizedUsersService implements UserDetailsService {
+
+        @Autowired
+        UserRepository userRepo;
+
+        /**
+         * @see UserDetailsService#loadUserByUsername(String)
+         */
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            Users users = userRepo.findById(username).orElse(null);
+
+            if (users == null || username.length() == 0) {
+                throw new UsernameNotFoundException(String.format(Locale.getDefault(), "Requested username ( %s ) not found.", username));
+            }
+
+            return new AuthorizedUsers(users);
+        }
+
+        @Transactional
+        public void saveUsers(Users users) {
+            userRepo.save(users);
+        }
+
+        public List<Users> findUsers(UserSearchConditionForm searchConditions) {
+            MySpecificationAdapter<Users> saUsers = new MySpecificationAdapter<>(Users.class);
+
+            String username = CommonsStringUtils.isNullOrWhiteSpace(searchConditions.getUsername()) ? null : searchConditions.getUsername();
+            String displayName = CommonsStringUtils.isNullOrWhiteSpace(searchConditions.getDisplayName()) ? null : searchConditions.getDisplayName();
+            String mailAddress = CommonsStringUtils.isNullOrWhiteSpace(searchConditions.getMailAddress()) ? null : searchConditions.getMailAddress();
+            Boolean enabledUser = searchConditions.getLimitEnabledUser() ? Boolean.TRUE : null;
+
+            return userRepo.findAll(Specifications.where(saUsers.contains("username", username))
+                    .and(saUsers.contains("displayName", displayName))
+                    .and(saUsers.contains("mailAddress", mailAddress))
+                    .and(saUsers.eq("enabled", enabledUser)));
+        }
+
+        public Users findUserById(String username) {
+            return userRepo.findById(username).orElse(null);
+        }
+    }
 }
